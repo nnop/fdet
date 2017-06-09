60717285ac857c945ff544e6ea424cd333be3ff0
ih4cku <ih4cku@gmain.com>
2017-06-10 01:29:36 +0800

    add draw_bbox()

--- lib/vis.py
+++ lib/vis.py
@@ -1,6 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+def draw_bbox(ax, bbox, text='', color='r', linewidth=2):
+    xmin, ymin, xmax, ymax = bbox
+    coords = (xmin, ymin), xmax-xmin+1, ymax-ymin+1
+    ax.add_patch(plt.Rectangle(*coords, fill=False, 
+            edgecolor=color, linewidth=linewidth))
+    ax.text(xmin, ymin, text, bbox={'facecolor': color, 'alpha': 0.5})
+
 def show_detection(image, bboxes, labelmap=[], threshold=0.0):
     """
     bboxes shape: (N, 7)
