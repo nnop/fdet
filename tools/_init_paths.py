388a7b9031a105c8561acd962257a9fcbbaebebd
ih4cku <ih4cku@gmain.com>
2017-06-15 13:16:35 +0800

    fix init path bug

--- tools/_init_paths.py
+++ tools/_init_paths.py
@@ -1,11 +1,12 @@
-from os import path
+import os.path as osp
 import sys
 
 def add_path(p):
-    if path not in sys.path:
+    if p not in sys.path:
         sys.path.insert(0, p)
 
 # add lib path
-this_dir = path.split(path.realpath(__file__))[0]
-lib_dir = path.normpath(path.join(this_dir, '..', 'lib'))
+this_dir = osp.split(osp.realpath(__file__))[0]
+lib_dir = osp.normpath(osp.join(this_dir, '..', 'lib'))
+assert osp.isdir(lib_dir), '{} not exists.'.format(lib_dir)
 add_path(lib_dir)
