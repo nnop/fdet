7eb7310134250838e3ee74f4e94460adbbf60ef7
ih4cku <ih4cku@gmain.com>
2017-06-16 10:32:30 +0800

    change os.path as osp

--- tools/select_samples.py
+++ tools/select_samples.py
@@ -3,35 +3,31 @@
 import argparse
 import random
 import os
-from os import path
+import os.path as osp
 import fnmatch
 import shutil
 
-def make_if_not_exists(folder):
-    if not path.isdir(folder):
-        os.makedirs(folder)
-
 def get_sub_samples(data_dir, num):
-    image_dir = path.join(data_dir, 'Frame')
+    image_dir = osp.join(data_dir, 'Frame')
     samples = fnmatch.filter(os.listdir(image_dir), '*.jpg')
-    samples = [path.splitext(fn)[0] for fn in samples]
+    samples = [osp.splitext(fn)[0] for fn in samples]
     sub_samples = random.sample(samples, num)
     return sub_samples
 
 def copy_samples(src_root, dst_root, samples, save_dir):
-    src_image_dir = path.join(src_root, 'Frame')
-    src_xml_dir = path.join(src_root, 'xml')
-    assert path.isdir(src_image_dir)
-    assert path.isdir(src_xml_dir)
-    dst_image_dir = path.join(dst_root, 'Frame')
-    dst_xml_dir = path.join(dst_root, 'xml')
+    src_image_dir = osp.join(src_root, 'Frame')
+    src_xml_dir = osp.join(src_root, 'xml')
+    assert osp.isdir(src_image_dir)
+    assert osp.isdir(src_xml_dir)
+    dst_image_dir = osp.join(dst_root, 'Frame')
+    dst_xml_dir = osp.join(dst_root, 'xml')
     make_if_not_exists(dst_image_dir)
     make_if_not_exists(dst_xml_dir)
     for name in samples:
-        src_image_p = path.join(src_image_dir, name+'.jpg')
-        dst_image_p = path.join(dst_image_dir, name+'.jpg')
-        src_xml_p = path.join(src_xml_dir, name+'.xml')
-        dst_xml_p = path.join(dst_xml_dir, name+'.xml')
+        src_image_p = osp.join(src_image_dir, name+'.jpg')
+        dst_image_p = osp.join(dst_image_dir, name+'.jpg')
+        src_xml_p = osp.join(src_xml_dir, name+'.xml')
+        dst_xml_p = osp.join(dst_xml_dir, name+'.xml')
         shutil.copyfile(src_image_p, dst_image_p)
         shutil.copyfile(src_xml_p, dst_xml_p)
         print name
