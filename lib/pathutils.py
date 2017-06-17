e16ce43e58443a85414f7f8f72778369285a0d7e
ih4cku <ih4cku@gmain.com>
2017-06-17 12:49:47 +0800

    update pathutils

--- lib/pathutils.py
+++ lib/pathutils.py
@@ -4,4 +4,10 @@ import os.path as osp
 def make_if_not_exists(folder):
     if not osp.isdir(folder):
         os.makedirs(folder)
+        print 'makedirs {}'.format(folder)
 
+def check_f_exists(f):
+    assert osp.isfile(f), '{} not exists'.format(f)
+
+def check_d_exists(d):
+    assert osp.isdir(d), '{} not exists'.format(d)
